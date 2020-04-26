1dac82eef9059d1efc2e5a68c5dba91063dc63bc
Igor Dias <igordiasth@gmail.com>
2020-04-26 18:57:21 +1200

    method get and post, able to list and create

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,43 @@
+import { getCustomRepository } from 'typeorm';
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, value, type }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionRepository);
+
+    // const { total } = transactionsRepository.getBalance();
+
+    // if (type === 'outcome' && total < value) {
+    //   throw new AppError('You do not have enough balance');
+    // }
+
+    // const checkCategoryExists = await transactionsRepository.findOne({
+    //   where: { category },
+    // });
+
+    // if (checkCategoryExists) {
+    //   throw new AppError('Category already exist.');
+    // }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
